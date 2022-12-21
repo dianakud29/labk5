8e74856e3c32966d662b16e9f54fd9669a2a1f68
Diank29 <diana.kudenchuk.ir.2022@lpnu.ua>
2022-12-21 19:11:29 +0200

    review adding labk5.cpp

--- labk5.cpp
+++ labk5.cpp
@@ -2,72 +2,63 @@
 #include <string>
 using namespace std;
 
-class Fish
-{
+class Fish{
 
 public:
-    std::string name, preferredFood;
-    int age, species;
-    double size, neededSpace;
-    bool isAggressive;
+    string name, preferred_food, species;
+    int age;
+    double size, needed_space;
+    bool is_aggressive;
 
-    void init(string name, int age, int species, double size, string preferredFood, bool isAggressive, double neededSpace)
-    {
+    void init(string name, int age, string species, double size, string preferred_food, bool is_aggressive, double needed_space){
         this->name = name;
         this->age = age;
         this->species = species;
         this->size = size;
-        this->preferredFood = preferredFood;
-        this->isAggressive = isAggressive;
-        this->neededSpace = neededSpace;
+        this->preferred_food = preferred_food;
+        this->is_aggressive = is_aggressive;
+        this->needed_space = needed_space;
     }
 };
 
-class Aquarium
-{
+class Aquarium{
 private:
-    double totalVolume;
-    int freeSpace;
+    double total_volume;
+    int free_space;
     Fish fish[3];
     // Fish arr[];
 
 public:
-    Aquarium(double totalVolume)
+    Aquarium(double total_volume)
     {
-        this->totalVolume = totalVolume;
-        this->freeSpace = totalVolume;
+        this->total_volume = total_volume;
+        this->free_space = total_volume;
     }
 
-    void addFish(Fish f[8])
-    {
-        for (int i = 0; i < 3; i++)
-        {
+    void addFish(Fish f[8]){
+        for (int i = 0; i < 3; i++){
             int r1 = 0;
             r1 =rand() % 8;
             this->fish[i] = f[r1];
-            this->freeSpace = this->freeSpace - f[r1].neededSpace;
+            this->free_space = this->free_space - f[r1].needed_space;
 
         }
     }
 
-    Fish *getAllFish()
-    {
+    Fish *get_all_fish(){
         return this->fish;
     }
 
-    double getFreeSpace()
-    {
-        return this->freeSpace;
+    double get_free_space(){
+        return this->free_space;
     }
 
-    double getTotalVolume()
-    {
-        return this->totalVolume;
+    double get_total_volume(){
+        return this->total_volume;
     }
 };
 
-int main()
-{
+int main(){
 
     Fish fish[8];
     fish[0].init("fName1", 1, 1, 2.3, "meet", false, 0.3);
@@ -79,18 +70,17 @@ int main()
     fish[6].init("fName7", 1, 3, 1.3, "meet", true, 5.3);
     fish[7].init("fName8", 1, 3, 1.3, "meet", false, 3.3);
 
-    Aquarium a(33.5);
+    Aquarium first_aquarium(33.5);
 
-    a.addFish(fish);
-    double fs = a.getFreeSpace();
+    first_aquarium.addFish(fish);
+    double fs = first_aquarium.get_free_space();
 
-    cout << "free space - " + std::to_string(fs) + " Aquarium space - " + std::to_string(a.getTotalVolume()) << endl;
+    cout << "free space - " + to_string(fs) + " Aquarium space - " + to_string(a.get_total_volume()) << endl;
 
-    Fish* f = a.getAllFish();
+    Fish* f = first_aquarium.get_all_fish();
 
     cout << "List of fish in Aquarium:" << endl;
-    for (int ii = 0; ii < 5; ii++)
-    {
+    for (int ii = 0; ii < 5; ii++){
         cout << f[ii].name << endl;
     }
 }
